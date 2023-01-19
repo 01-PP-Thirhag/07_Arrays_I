fb9ba860d480cb4b5ae8eb5525686ce4511604b5
Thirhag <meigraf@hotmail.de>
2023-01-19 11:56:02 +0100

    1

--- script_01.js
+++ script_01.js
@@ -3,11 +3,28 @@
 /* Die Satzbau-Maschine | Arrays */
 
 /*** 02. Theorie: Array */
+let arr;
+arr =[];
 
+
+output(arr)
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-
+// output(getSentence("Ich","bin","das","krasse","Miez","Miau"))
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP = " ";
+    const PUNCT =".";
+    let str =   word1 + GAP + 
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                word5 + GAP +
+                word6 + 
+                PUNCT;
+    
+    return str;  
+}
 
 
 
